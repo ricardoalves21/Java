e3f6a9064019768140ae487e66cb3ac75ceab7c2
Ricardo Alves <dev.rical21@gmail.com>
2021-05-05 15:58:46 -0300

    Segundo commit Java

--- com_orientacao_objeto/src/aplicacao/Programa.java
+++ com_orientacao_objeto/src/aplicacao/Programa.java
@@ -25,11 +25,8 @@ public class Programa {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p = x.b) * (p = x.c));
-		
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p = y.a) * (p - y.b) * (p - y.c));
+		double areaX = x.area();		
+		double areaY = y.area();
 		
 		System.out.printf("Triangulo X area: %.2f%n", areaX);
 		System.out.printf("Triangulo Y area: %.2f%n", areaY);
--- com_orientacao_objeto/src/entidades/Triangulo.java
+++ com_orientacao_objeto/src/entidades/Triangulo.java
@@ -1,9 +1,19 @@
 package entidades;
 
 public class Triangulo {
+	
+	// DECLARANDO OS ATRIBUTOS DA CLASSE
 
 	public double a;
 	public double b;
 	public double c;
+	
+	// DECLARANDO O MÉTODO DA CLASSE
+
+	public double area() {
+		double p = (a + b + c) / 2.00;
+		return Math.sqrt(p * (p - a) * (p - b ) * (p - c));
+		
+	}	
 	
 }
