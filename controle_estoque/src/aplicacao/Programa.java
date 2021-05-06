ecf2a3ea3a10f3f23efd427317a9e2fedec73411
Ricardo Alves <dev.rical21@gmail.com>
2021-05-06 11:33:19 -0300

    Commit Java 06-05-21 11:32h

--- controle_estoque/src/aplicacao/Programa.java
+++ controle_estoque/src/aplicacao/Programa.java
@@ -15,14 +15,35 @@ public class Programa { // descrição da classe
 		Produto produto = new Produto(); // instanciando o objeto produto da classe Produto
 		
 		System.out.println("ENTRE COM O PRODUTO"); // título da aplicação
-		System.out.println("Nome: "); // solicitando ao usuário o nome do produto
+		System.out.println();
+		
+		System.out.print("Nome: "); // solicitando ao usuário o nome do produto
 		produto.nome = sc.nextLine(); // guardando a resposta do usuário no objeto produto no atributo nome
-		System.out.println("Preço: ");
+		
+		System.out.print("Preço: ");
 		produto.preco = sc.nextDouble();
-		System.out.println("Quantidade: ");
+		
+		System.out.print("Quantidade: ");
 		produto.quantidade = sc.nextInt();
 		
-		System.out.println(produto); // quando coloco apenas o nome do objeto no println o Java já entende que a resposta será do tipo String.
+		System.out.println();
+		System.out.println("Dados do produto: " + produto); // quando coloco apenas o nome do objeto no println o Java já entende que a resposta será do tipo String.
+		
+		System.out.println();
+		System.out.print("Entre com a quantidade de produtos que será adicionada ao estoque: ");
+		int qtdEntrada = sc.nextInt();
+		produto.adicionarProduto(qtdEntrada);
+		
+		System.out.println();
+		System.out.println("Atualização de estoque: " + produto);
+		
+		System.out.println();
+		System.out.print("Entre com a quantidade de produtos que será retirada do estoque: ");
+		int qtdSaida = sc.nextInt();
+		produto.removerProduto(qtdSaida);
+		
+		System.out.println();
+		System.out.println("Atualização de estoque: " + produto);
 		
 		sc.close();
 	}
