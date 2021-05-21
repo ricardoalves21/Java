c3337cacb7bdfd5d84728f270dcb53e5f525587d
Ricardo Alves <dev.rical21@gmail.com>
2021-05-20 22:25:05 -0300

    Commit Java 20-05-2021 22:25

--- arrays_listas/src/lista/Lista.java
+++ arrays_listas/src/lista/Lista.java
@@ -9,14 +9,21 @@ public class Lista {
 		
 		List<String> lista = new ArrayList<>(); // esta é a maneira de instanciar uma lista no Java
 		
-		lista.add("Ricardo");
+		lista.add("Ricardo"); // adicionando itens na lista instanciada
 		lista.add("Elias");
 		lista.add("Juliana");
 		lista.add("Henrique");
+		lista.add(2, "Josiane"); // para adicionar um item na posição 2 da lista	
+		lista.remove("Henrique"); // para remover um item da lista
+		lista.remove(1); // para remover um item da lista indicando a posição do item
+		lista.removeIf(x -> x.charAt(0) == 'R'); // para remover um item incluindo condições específicas para isso (predicado)
 		
-		for (String x : lista) {
+		System.out.println(lista.size()); // imprimindo o tamanho da lista
+		for (String x : lista) { // usando for each para varrer a lista e apresentar os itens contidos nela
 			System.out.println(x);
 		}
+		System.out.println("-----------------------------");
+
 
 	}
 
