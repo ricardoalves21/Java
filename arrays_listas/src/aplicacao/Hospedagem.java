b7baf4d883d2e3fe15e7ae9e4b1cd785fa4ec0fc
Ricardo Alves <dev.rical21@gmail.com>
2021-05-21 12:41:03 -0300

    Commit Java 21-05-2021 12:41

--- arrays_listas/src/aplicacao/Hospedagem.java
+++ arrays_listas/src/aplicacao/Hospedagem.java
@@ -12,13 +12,18 @@ public class Hospedagem {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int n_quartos = 10;
+		int n_quartos = 10;		
 		Hotel[] quarto = new Hotel[n_quartos];
-			
+		
 		System.out.println("XXXXXXXXXX HOSPEDAGEM XXXXXXXXXX");
 		System.out.println();
 		
-		for (int i=0; i<quarto.length; i++) {
+		System.out.print("Quantos quartos serão alugados? ");
+		int hospedagens = sc.nextInt();
+			
+
+		
+		for (int i=0; i<hospedagens; i++) {
 			
 			System.out.print("Nome do hospede: ");
 			String nome = sc.next();
@@ -31,26 +36,23 @@ public class Hospedagem {
 			
 			quarto[i] = new Hotel(nome, email, apto);
 			
-			System.out.println();
-			System.out.print("Deseja hospedar mais pessoas [s/n] ? ");
-			String resp = sc.next();
-			
-			if ("n".contentEquals(resp)) {
-				i = 10;
 			}
 			
-			System.out.println();
-			
-		}	
+		System.out.println();
 		
 		System.out.println("********** RELATORIO DE HOSPEDAGEM **********");
 		System.out.println();
 		
-		for (int i=0; i<quarto.length; i++) {
-			System.out.println(quarto[i]);
+		// int ord = 0;
+				
+		for (int i=0; i<hospedagens; i++) {
+			for (int j=0; j<hospedagens; j++) {
+				System.out.println(quarto[j].getApto());
+			}
+			
+			System.out.println(quarto[i].getApto());
 		}
 		
 		sc.close();
 	}
-
 }
